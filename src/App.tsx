e6ee7cab62e1e3d4e05db29d765876c4cc1cf05f
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-18 21:25:38 +0100

    usestate imp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import UseState from "./UseState";
+
 function App() {
   return (
     <div>
       <h1>Use state</h1>
+      <UseState />
     </div>
   );
 }
--- src/UseState.tsx
+++ src/UseState.tsx
@@ -1,6 +1,19 @@
 import { useState } from "react";
 
 export default function UseState() {
-  const [arr, arrSet] = useState([]);
-  return <div></div>;
+  const [arr, arrSet] = useState<number[]>([]);
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            arrSet([...arr, arr.length + 1]);
+          }}
+        >
+          Add to array
+        </button>
+        {JSON.stringify(arr)}
+      </div>
+    </div>
+  );
 }
