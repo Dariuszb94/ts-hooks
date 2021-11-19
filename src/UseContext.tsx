5294aea13a73890cd5af4304d838c19283a136a5
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-19 21:22:15 +0100

    context consumer

--- src/UseContext.tsx
+++ src/UseContext.tsx
@@ -1,7 +1,25 @@
+import { useContext } from "react";
+import { useState } from "react";
 import UserContext, { UserState } from "./store";
 const UseContext = () => {
-  const [user, userSet] = useState<UserState>({});
-  return <UserContext.Provider value={user}></UserContext.Provider>;
+  const [user, userSet] = useState<UserState>({
+    first: "Adam",
+    last: "Kowalski",
+  });
+  return (
+    <UserContext.Provider value={user}>
+      <Consumer />
+    </UserContext.Provider>
+  );
 };
 
 export default UseContext;
+
+const Consumer = () => {
+  const { first, last } = useContext<UserState>(UserContext);
+  return (
+    <div>
+      First:{first}, Last:{last}
+    </div>
+  );
+};
